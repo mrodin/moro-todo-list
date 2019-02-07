4803ff80302694bafa045f8b9294368248daf6df
Martin Rodin <rodin.martin@gmail.com>
2019-02-07 22:21:15 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Starter React App
+      </div>
+    );
+  }
+}
+
+export default App;
