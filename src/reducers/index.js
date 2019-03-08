626b8517d264d3053364df595b1ec2f776e74493
Martin Rodin <martin.rodin@morosystems.cz>
2019-03-08 12:51:12 +0100

    add loading bar

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux'
+import { loadingBarReducer } from 'react-redux-loading'
+
 import visibilityFilter from './visibilityFilter'
 import todos from './todos'
 
 export default combineReducers({
   visibilityFilter,
   todos,
+  loadingBar: loadingBarReducer
 })
